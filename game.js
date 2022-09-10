edd7e8e518bd15fd5a0af4a79fd6375a25960606
nrxnico <107650850+nrxnico@users.noreply.github.com>
2022-09-10 11:54:55 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -44,13 +44,17 @@ function checkAnswer(currentLevel) {
 
     } else {
 
+
+      playSound("wrong");
+
+      //2. In the styles.css file, there is a class called "game-over", apply this class to the body of the website when the user gets one of the answers wrong and then remove it after 200 milliseconds.
       $("body").addClass("game-over");
-      setTimeout(function (){
+      setTimeout(function () {
         $("body").removeClass("game-over");
       }, 200);
-      $("h1").text("Game Over, Press Any Key to Restart")
 
-      startOver();
+      //3. Change the h1 title to say "Game Over, Press Any Key to Restart" if the user got the answer wrong.
+      $("#level-title").text("Game Over, Press Any Key to Restart");
 
     }
 
